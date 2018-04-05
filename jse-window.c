e3a21fd5005eb3396dd481644d5d463fdfeaf60b
Volker Sobek <reklov@live.com>
2018-04-06 00:38:17 +0200

    c

--- jse-window.c
+++ jse-window.c
@@ -430,10 +430,11 @@ on_transition_stopped_cb (ClutterActor *actor,
 
   if (win->current_actor)
     {
+      g_debug ("  removing old_actor %p", win->current_actor);
       clutter_actor_remove_child (win->stage, win->current_actor);
-      g_debug ("  removing old_actor %p", actor);
     }
 
+  g_debug ("  setting current_actor %p", win->current_actor);
   win->current_actor = actor;
 
   g_signal_handlers_disconnect_by_func (actor, on_transition_stopped_cb, data);
